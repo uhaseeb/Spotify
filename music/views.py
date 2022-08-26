f08ef2facc1ad22fed2bf4794e126ddfe116f039
uhaseeb <uhaseeb731@gmail.com>
2022-08-26 11:41:00 +0500

    done with the search module

--- music/views.py
+++ music/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+from .forms import SearchForm
+from .models import Track, Album, Artist, Genre, Playlist
+from django.http import HttpResponse
 
 class IndexView(View):
-    pass
+    template_name = 'index.html'
+    form = SearchForm
 
+    def post(self, request):
+        search = request.POST['search']
+        form = SearchForm(request.POST)
+        if Track.objects.filter(track_name=search):
+            songs = Track.objects.filter(track_name=search)
+            is_song = True
+            context = {'songs': songs, 'is_song': is_song, 'form': form}
+            return render(request, 'index.html', context)
+
+        elif Album.objects.filter(album_name=search):
+            albums = Album.objects.filter(album_name=search)
+            is_album = True
+            context = {'albums': albums, 'is_album': is_album, 'form': form}
+            return render(request, 'index.html', context)
+
+        elif Artist.objects.filter(artist_name=search):
+            artists = Artist.objects.filter(artist_name=search)
+            is_artist = True
+            context = {'artists': artists, 'is_artist': is_artist, 'form': form}
+            return render(request, 'index.html', context)
+
+        else:
+            return HttpResponse("no record found")
+
+    def get(self, request):
+        form = SearchForm()
+
+        return render(request, 'index.html', {'form': form})
--- spotify/urls.py
+++ spotify/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music', include('music.urls'))
+    path('music/', include('music.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
